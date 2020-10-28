9e2123c1f2bb17c50850bdf1383816b4a3a34eec
OnlyEduardo <eduardoleal.contact@gmail.com>
2020-10-27 21:20:03 -0300

    add battery

---
+++ src/com/swellshinider/instruments/Battery.java
@@ -0,0 +1,21 @@
+package com.swellshinider.instruments;
+
+import com.swellshinider.instruments.enumerators.Metal;
+import com.swellshinider.instruments.enumerators.TradeMark;
+import com.swellshinider.instruments.enumerators.Type;
+import com.swellshinider.instruments.enumerators.Wood;
+import com.swellshinider.instruments.specs.PercussionInstruments;
+
+public class Battery extends PercussionInstruments {
+
+    public Battery(long serial, float price, TradeMark tradeMark, Metal metalPart, Wood bodyWood, Type instruementsType) {
+        super(serial, price, tradeMark, metalPart, bodyWood, instruementsType);
+    }
+
+    @Override
+    public String toString() {
+        return "Bateria " + tradeMark +
+                ", materiais: " + getBodyWood() + " e " + getMetalPart() +
+                " (R$"+formatter.format(price)+")";
+    }
+}
--- src/com/swellshinider/instruments/specs/PercussionInstruments.java
+++ src/com/swellshinider/instruments/specs/PercussionInstruments.java
@@ -2,17 +2,24 @@ package com.swellshinider.instruments.specs;
 
 import com.swellshinider.instruments.enumerators.Metal;
 import com.swellshinider.instruments.enumerators.TradeMark;
+import com.swellshinider.instruments.enumerators.Type;
 import com.swellshinider.instruments.enumerators.Wood;
 
 public class PercussionInstruments extends Instruments {
 
     private final Metal metalPart;
     private final Wood bodyWood;
+    private final Type instrumentsType;
 
-    public PercussionInstruments(long serial, float price, TradeMark tradeMark, Metal metalPart, Wood bodyWood) {
+    public PercussionInstruments(long serial, float price, TradeMark tradeMark, Metal metalPart, Wood bodyWood, Type instruementsType) {
         super(serial, price, tradeMark, "Percurssão");
         this.metalPart = metalPart;
         this.bodyWood = bodyWood;
+        this.instrumentsType = instruementsType;
+    }
+
+    public Type getInstrumentsType(){
+        return instrumentsType;
     }
 
     public Metal getMetalPart() {
