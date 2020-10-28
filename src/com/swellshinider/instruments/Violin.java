0041fc8e505c29fe91a622cd6a598acf7a7a236a
OnlyEduardo <eduardoleal.contact@gmail.com>
2020-10-27 21:23:51 -0300

    add violin

---
+++ src/com/swellshinider/instruments/Violin.java
@@ -0,0 +1,23 @@
+package com.swellshinider.instruments;
+
+import com.swellshinider.instruments.enumerators.TradeMark;
+import com.swellshinider.instruments.enumerators.Type;
+import com.swellshinider.instruments.enumerators.Wood;
+import com.swellshinider.instruments.specs.StringInstruments;
+
+public class Violin extends StringInstruments {
+
+    public Violin(long serial, float price, TradeMark tradeMark, Wood backWood, Wood topWood, Type instrumentType) {
+        super(serial, price, tradeMark, backWood, topWood, instrumentType);
+    }
+
+    public int getStringQuantity() { return 6; }
+
+    @Override
+    public String toString() {
+        return "Violino " + tradeMark +
+                ", " + getStringQuantity() +
+                " cordas, materiais: " + getTopWood() + " e " + getBackWood() +
+                " (R$"+formatter.format(price)+")";
+    }
+}
